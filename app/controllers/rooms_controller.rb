79147836edadcd27ad5e5ff484e90d829f69e399
kazusystemakure <70381817+kazusystemakure@users.noreply.github.com>
2020-09-06 15:42:30 +0900

    メッセージの表示

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,4 @@
 class RoomsController < ApplicationController
-  def index
-  end
-  
   def new
     @room = Room.new
   end
@@ -14,8 +11,9 @@ class RoomsController < ApplicationController
       render :new
     end
   end
-  
+
   private
+
   def room_params
     params.require(:room).permit(:name, user_ids:[])
   end
